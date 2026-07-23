572f9015a483f643e360b2bc96dd3aa934498895
xueyang <313867808@qq.com>
2026-07-23 11:51:20 +0800

    chore: 删除废弃的 FormGroup 和 List 组件
    - FormGroup 功能已被 Form 组件替代
    - List 组件功能已被 CellGroup + Cells 组件替代
    - 清理相关测试和导出

--- packages/components/src/form-group/index.ts
+++
@@ -1,9 +0,0 @@
-import type { App } from 'vue'
-import WeuiFormGroup from './form-group.vue'
-
-WeuiFormGroup.install = (app: App) => {
-  app.component(WeuiFormGroup.name!, WeuiFormGroup)
-}
-
-export { WeuiFormGroup }
-export type { WeuiFormGroupProps } from './form-group.vue'
--- packages/components/src/list/index.ts
+++
@@ -1,9 +0,0 @@
-import type { App } from 'vue'
-import WeuiList from './list.vue'
-
-WeuiList.install = (app: App) => {
-  app.component(WeuiList.name || 'WeuiList', WeuiList)
-}
-
-export { WeuiList }
-export type { WeuiListProps } from './list.vue'
